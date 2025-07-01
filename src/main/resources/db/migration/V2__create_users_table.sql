fcfe5694a3b7692c7edb4b7c393b3fd3959f1f49
kimgayeons <4402aaa@gmail.com>
2025-07-01 13:22:17 +0900

    6월 19일(목) > V2__create_users_table.sql문 수정

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE user
 (
     id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name      VARCHAR(50)  NOT NULL,
+    name          VARCHAR(50)  NOT NULL,
     email         VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    created_at    DATETIME              DEFAULT CURRENT_TIMESTAMP
+    created_at    DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at    DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
